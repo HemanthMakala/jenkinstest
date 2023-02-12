92b0b9cf0473faf4147a507117c91e41d9becc1f
Hemanth paul <hemanthpaulmakala@gmail.com>
2023-02-12 21:02:19 +0530

    added restful web service

---
+++ src/main/java/com/jenkins/jenkinstest/TestController.java
@@ -0,0 +1,16 @@
+package com.jenkins.jenkinstest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	@RequestMapping("/test")
+	public String testMethod() {
+		return "Hello Bunny";
+		
+	}
+
+}
