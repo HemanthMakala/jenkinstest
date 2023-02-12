25bcb6e8a5ba7d9a183753d5b7c6e3b776e46a09
Hemanth paul <hemanthpaulmakala@gmail.com>
2023-02-12 20:07:54 +0530

    added test case

--- src/test/java/com/jenkins/jenkinstest/JenkinstestApplicationTests.java
+++ src/test/java/com/jenkins/jenkinstest/JenkinstestApplicationTests.java
@@ -1,13 +1,23 @@
 package com.jenkins.jenkinstest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinstestApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinstestApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+
+		logger.info("Test case executing...");
+		assertEquals(true, true);
+
 	}
 
 }
